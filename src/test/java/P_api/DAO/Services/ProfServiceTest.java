480555959ef0b138fb46442f236557fb679e4c12
isaias <pauloisaias54321@gmail.com>
2026-02-06 17:47:35 -0300

    Update ProfServiceTest.java

--- src/test/java/P_api/DAO/Services/ProfServiceTest.java
+++ src/test/java/P_api/DAO/Services/ProfServiceTest.java
@@ -5,9 +5,12 @@ import P_api.Model.Professor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@ActiveProfiles("test")
 class ProfServiceTest {
     @Autowired
     ProfService profService;
